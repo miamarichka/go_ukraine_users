4497b5ea3ef4d8d53571c3dd0de394871814f3f4
miamarichka <ilchyshyn.marichka@gmail.com>
2023-07-25 16:11:15 +0300

    renaming

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,6 @@
 const validateUserData = require('./validateUserData');
 const authenticate = require('./authenticate');
-const ImgUploader = require('./ImgUploader');
+const ImgUploader = require('./imgUploader');
 const checkPassword = require('./checkPassword')
 
 module.exports = {
